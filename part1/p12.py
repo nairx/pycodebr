cd74cf0fa479d4e558cc889fc5bd70f3a8504d02
nairx <nairxprv@gmail.com>
2025-10-06 14:09:02 +0530

    updated

---
+++ part1/p12.py
@@ -0,0 +1,12 @@
+# for i in range(5):
+#     print(i)
+    
+# for i in range(1,5):
+#     print(i)
+
+for i in range(1,8,2):
+    print(i)
+    if i==5:
+        break
+else:
+    print("Program completed successfully")
