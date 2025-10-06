4a09e5901253ac305216d8f19e8a4d4338097b0b
nairx <nairxprv@gmail.com>
2025-10-06 12:40:56 +0530

    Updated

--- part1/p1.py
+++ part1/p1.py
@@ -1,9 +1,14 @@
 a=10
 b=20
 name="Sajith"
+surname="Paraveetil"
 result=True
 c=a+b
 print(c)
 print(name)
+print(surname)
+print(name,surname)
+print(name,end=" - ")
+print(surname)
 print(result)
 
---
+++ part1/p2.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+favorite_color = "yellow"
+# print("Hello",name)
+print("Hello" + "-" + name + " Favorite Color is " + favorite_color)
+print(f"Hello-{name} Favorite Color is {favorite_color}")
